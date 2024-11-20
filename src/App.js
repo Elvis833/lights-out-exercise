48d4072bb4a7e8879d106438650553f0469a186c
Student2 <student2@Students-MacBook-Pro.local>
2024-11-20 13:56:36 +1300

    Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import Header from './Header';
 function App() {
   return (
     <div className="App">
-      {/* Your components will go here */}
+      <Header />
     </div>
   );
 }
